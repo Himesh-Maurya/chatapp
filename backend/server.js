0cf2e273a8640dae48c0a73a6940d0106bd3c166
Himesh-Maurya <himeshmauurya@gmail.com>
2022-12-15 08:22:19 +0530

    change origin

--- backend/server.js
+++ backend/server.js
@@ -68,9 +68,9 @@ const server1 = app.listen(
 const io = new Server(server1, {
   pingTimeout: 60000,
   cors: {
-    // origin: "http://localhost:3000",
+    origin: "http://localhost:3000",
     // origin: "https://chatapp-cmoa0r9jb-himesh-maurya.vercel.app/",
-    origin: "*",
+    // origin: "*",
     // credentials: true,
   },
 });
